697accc15c6e3dec029f66dbd7c5495772d35f49
Vimala Murthy <vimala_murthy@yahoo.com>
2020-06-02 21:37:13 -0500

    Adding code to counter

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function ButtonPad() {
-    return (
-        <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
-        </div>
-    )
+  return (
+    <div>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </div>
+  );
 }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
-import ButtonPad from './ButtonPad';
+//import ButtonPad from './ButtonPad';
 
+class Counter extends Component {
+  state = { count: 0 };
 
-function Counter() {
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    let Decr = this.state.count;
+    if (Decr > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
     return (
-        <div className="counter">
-            <Display/>
-            <ButtonPad/>
-        </div>
-    )
+      <div className='counter'>
+        <span> {this.formatCount()}</span>
+        <Display />
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
 }
 
 export default Counter;
